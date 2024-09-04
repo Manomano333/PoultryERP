3ca8e1634525f076f4815d6ab93f15c617dabcea
Manoranjan M <106269294+Manomano333@users.noreply.github.com>
2024-09-04 18:47:33 +0530

    Add files via upload

---
+++ PoultryErp/src/main/java/org/example/poultryerp/PoultryErpApplication.java
@@ -0,0 +1,13 @@
+package org.example.poultryerp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoultryErpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoultryErpApplication.class, args);
+    }
+
+}
---
+++ PoultryErp/src/test/java/org/example/poultryerp/PoultryErpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.poultryerp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoultryErpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
